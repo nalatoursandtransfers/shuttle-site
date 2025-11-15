2a84b9775fa40beb125cd3276f29c85850b80625
Amahle Khoza <amahlekhoza78@gmail.com>
2025-11-15 22:52:23 +0200

    Update website changes

--- app.js
+++ app.js
@@ -3,24 +3,36 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, on
 import { getFirestore, collection, addDoc, setDoc, doc, query, where, getDocs, orderBy } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "REPLACE_API_KEY",
-    authDomain: "REPLACE_AUTH_DOMAIN",
-    projectId: "REPLACE_PROJECT_ID",
-    storageBucket: "REPLACE_STORAGE_BUCKET",
-    messagingSenderId: "REPLACE_SENDER_ID",
-    appId: "REPLACE_APP_ID"
+    apiKey: "AIzaSyDBaixqzu2Bgemqdw9mycZ-FHlbGer5fDg",
+    authDomain: "shuttle-site-915e1.firebaseapp.com",
+    projectId: "shuttle-site-915e1",
+    storageBucket: "shuttle-site-915e1.firebasestorage.app",
+    messagingSenderId: "726821391442",
+    appId: "1:726821391442:web:77cc38be770e574386e38b",
+    measurementId: "G-NHGWQYHHHV"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+onAuthStateChanged(auth, (user) => {
+    const authArea = document.getElementById("auth-area");
+    if (user) {
+        authArea.textContent = `Hello, ${user.email} | `;
+    } else {
+        authArea.textContent = '';
+    }
+});
+
+const btnBook = document.getElementById("btn-book");
+
 if (btnBook) {
     btnBook.addEventListener("click", async () => {
         const pickup = document.getElementById("pickup").value.trim();
         const dropoff = document.getElementById("dropoff").value.trim();
-        const when = document.getElementById("when").value();
-        const passengers = document.getElementById("passengers").value();
+        const when = document.getElementById("when").value;
+        const passengers = document.getElementById("passengers").value;
         const notes = document.getElementById("notes").value;
 
         if (!auth.currentUser) return alert("You must be logged in to make a booking.");
